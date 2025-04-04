80588b08fba9313e61903ba9a90478211779dbba
yatinbisht76 <yatin@pcgamesupply.com>
2025-04-04 12:47:50 +0530

    .env
    .env

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dlenutttepqdlyawlxyp.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsZW51dHR0ZXBxZGx5YXdseHlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2NjAwMjcsImV4cCI6MjA1OTIzNjAyN30.p25c9KAqb03wXSCOAk2MT1YP5VS3QjOGMwk5sFiPD8A'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error('Missing Supabase environment variables')
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
